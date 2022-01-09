e567e42fdd2f1c9f3df7de5b005ef3fb01697bf4
vitaszhuo <2510776295@qq.com>
2022-01-09 11:42:28 +0800

    更新

--- springbootupload/src/main/java/com/upload/controller/FileUploadController.java
+++ springbootupload/src/main/java/com/upload/controller/FileUploadController.java
@@ -1,16 +1,22 @@
 package com.upload.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 /*
 * 文件上传
 * */
+
+@RestController
 public class FileUploadController {
-    @PostMapping("/fileUpload")
+    @RequestMapping("/fileUpload")
     public String fileUpload( MultipartFile file) throws Exception{
-        //System.out.println(file.getOriginalFilename());
-        //file.transferTo(new File("N:/"+file.getOriginalFilename()));
+        System.out.println(file.getOriginalFilename());
+        file.transferTo(new File("N:/"+file.getOriginalFilename()));
         return "ok";
     }
 
