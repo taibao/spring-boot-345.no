51c761dd2eb9b9bdd4a339354d84ff09de19913f
vitaszhuo <2510776295@qq.com>
2022-01-23 17:50:34 +0800

    更新

--- springbootfreemarker/src/main/java/com/vitas/controller/UsersController.java
+++ springbootfreemarker/src/main/java/com/vitas/controller/UsersController.java
@@ -1,15 +1,26 @@
 package com.vitas.controller;
 
 import com.vitas.pojo.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UsersController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/showInfo")
+    public String showInfo(){
+        return "ok";
+    }
+
 
     /*
     * 处理请求，返回数据
