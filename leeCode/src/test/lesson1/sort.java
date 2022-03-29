cc87fb1f1dd37a07be42601e4878749e568f7fd0
vitaszhuo <2510776295@qq.com>
2022-03-29 21:51:34 +0800

    排序算法

---
+++ leeCode/src/test/lesson1/sort.java
@@ -0,0 +1,72 @@
+package test.lesson1;
+
+import java.util.Arrays;
+
+public class sort {
+    public static void main(String[] args){
+       int[] arr = generateRandomArray(5,10);
+//       dd(arr);
+    //   int sum = digui(4,3); //递归求阶乘
+     //   System.out.println(sum);
+
+        selectionSort(arr);
+        dd(arr);
+
+        comparator(arr);
+        dd(arr);
+    }
+
+    //选择排序
+ public static void selectionSort(int[] arr){
+        if(arr == null || arr.length<2){
+            return;
+        }
+        int len = arr.length;
+        for(int i=0;i<len-1;i++ ){
+            for(int j=i+1;j<len;j++){
+                if(arr[i]>arr[j]){
+                    swap(arr,i,j);
+                }
+            }
+        }
+ }
+
+//数组排序
+public static void comparator(int[] arr){
+        Arrays.sort(arr);
+}
+
+
+ public static void swap(int[] arr, int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+ }
+
+
+    //数组生成器
+    public static int[] generateRandomArray(int maxSize, int maxValue){
+        int[] arr = new int[(int)((maxSize))];
+        for(int i=0;i<arr.length;i++){
+//            arr[i] = (int)((maxValue + 1)*Math.random()) - (int) (maxValue * Math.random());
+            arr[i] = (int)((maxValue + 1)*Math.random());
+        }
+        return arr;
+    }
+
+
+    public static void dd(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    //最简单递归求阶乘，可知递归要素1.结果 2.下一级条件，不可缺少
+    public static int digui(int sum,int i){
+        return i>1?digui(sum*i,i-1):sum; //条件要递减最后要返回
+    }
+
+
+
+
+}
--- leeCode/src/test/sort.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class sort {
-    public static void main(String[] args){
-        System.out.println(123);
-    }
-
-}
